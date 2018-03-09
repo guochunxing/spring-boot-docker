1321828f87bc6fc4addda7648411800cbe706fb7
chunxing <chunxing@fit2cloud.com>
2018-03-09 16:00:03 +0800

    init

---
+++ src/test/java/com/fit2cloud/compose/ComposeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fit2cloud.compose;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComposeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
